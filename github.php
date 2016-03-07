9eb280444a130458ed572829baa03f4888eff14d
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2016-03-07 23:31:16 +0100

    extract github calls to own class

---
+++ github.php
@@ -0,0 +1,41 @@
+<?php
+require_once('workflows.php');
+
+class Github {
+	private $repo;
+	private $token;
+	private $w;
+	
+	function __construct($repo, $token=null) {
+		$this->repo = $repo;
+		$this->token = $token;
+		$this->w = new Workflows();
+	}
+
+	public function getContent() {
+		return $this->query("repos/".$this->repo."/contents");
+	}
+
+	public function search($query) {
+		return $this->query("search/code", array("q" => "$query repo:".$this->repo));
+	}
+
+	private function query($resource, $params = null) {
+		$url = $this->getUrl($resource, $params);
+		$jsonResponse = $this->w->request($url, array(CURLOPT_USERAGENT => 'alfred'));
+		$response = json_decode($jsonResponse, true);
+		return $response;
+	}
+	
+	private function getUrl($resource, $params = null) {
+		$allParams = is_null($params) ? array() : $params;
+		if(!empty($this->token)) {
+			$allParams["access_token"] = $this->token;
+		}
+		
+		$url = sprintf("%s?%s", "https://api.github.com/".$resource, http_build_query($allParams));
+		return $url;
+	}
+}
+
+?>
--- nb.php
+++ nb.php
@@ -1,4 +1,7 @@
 <?php
+require_once('github.php');
+require_once('workflows.php');
+
 if(!ini_get( 'date.timezone')) {
 	$timezone = exec( 'tz=`ls -l /etc/localtime` && echo ${tz#*/zoneinfo/}' );
     ini_set( 'date.timezone', $timezone );
@@ -7,45 +10,34 @@ if(!ini_get( 'date.timezone')) {
 array_shift($argv);
 $query = trim($argv[0]);
 
-require_once('workflows.php');
 $w = new Workflows();
 
 $access_token = $w->get('token', 'settings.plist');
 $repo = $w->get('repo', 'settings.plist');
 
-$items = array();
+$gh = new Github($repo, $access_token);
 
 //Configuration error
 if(empty($repo)) {
-	$errorItem = array(
+	echo $w->toxml(array(
 	  	'uid' => 'missingRepo',
 		'title' => 'Repository not set',
 		'subtitle' => "use nb_conf to set repository",
 		'icon' => 'book-icon.png',
 		'valid' => 'no'
-	);
-	array_push($items, $errorItem);	
-
-	$w = new Workflows();
-	echo $w->toxml($items);
-		
+	));
 	exit(1);
 }
 
+$items = array();
+
+//Create new note
 if(strpos($query, "+") === 0) {
 	$searchCategory = trim($query, "+\t ");
-
 	$template = "---\ncreated: ".date("Y-m-d")."\ntags: []\n---\n\n";
 
-	$params = array();
-	if(!empty($access_token)) {
-		$params["access_token"] = $access_token;
-	}
-
-	$url = sprintf("%s?%s", "https://api.github.com/repos/".$repo."/contents", http_build_query($params));
-	$jsonResponse = $w->request($url, array(CURLOPT_USERAGENT => 'alfred'));
-	$response = json_decode($jsonResponse, true);
-
+	$response = $gh->getContent();
+	
 	foreach($response as $item) {
 		if($item['type'] == "dir" && $item['name'] != 'resources'
 		 && (empty($searchCategory) || stripos($item['name'], $searchCategory) > -1)) {
@@ -65,24 +57,12 @@ if(strpos($query, "+") === 0) {
 		}
 	}
 	
-	$w = new Workflows();
 	echo $w->toxml($items);
-
 	exit(0);
 }
 
 if(strlen($query) > 2) {
-	$query = "$query repo:$repo";
-	$params = array("q" => $query);
-	if(!empty($access_token)) {
-		$params["access_token"] = $access_token;
-	}
-
-	$url = sprintf("%s?%s", "https://api.github.com/search/code", http_build_query($params));
-
-	$jsonResponse = $w->request($url, array(CURLOPT_USERAGENT => 'alfred'));
-	$response = json_decode($jsonResponse, true);
-
+	$response = $gh->search($query);
 	foreach($response['items'] as $item) {
 		$itemUrl = "https://github.com/$repo/blob/master/".$item['path'];
 
@@ -110,7 +90,6 @@ if(empty($item)) {
 	array_push($items, $openRepo);	
 }
 
-$w = new Workflows();
 echo $w->toxml($items);
 
 ?>
